e4061da2d9a8ecbe3be73b85fbd7fb1e4e9150f6
Brett Smith <brett@logonbox.com>
2021-05-17 10:49:11 +0100

    Show transfer progress (trying to debug SSH package upload)

--- src/main/java/com/logonbox/maven/plugins/generator/UploadDeb.java
+++ src/main/java/com/logonbox/maven/plugins/generator/UploadDeb.java
@@ -11,6 +11,7 @@ import org.sonatype.inject.Description;
 
 import com.sshtools.client.sftp.SftpClientTask;
 import com.sshtools.client.sftp.TransferCancelledException;
+import com.sshtools.client.tasks.FileTransferProgress;
 import com.sshtools.common.permissions.PermissionDeniedException;
 import com.sshtools.common.sftp.SftpStatusException;
 import com.sshtools.common.ssh.SshException;
@@ -20,7 +21,7 @@ import com.sshtools.common.ssh.SshException;
  */
 @Mojo(defaultPhase = LifecyclePhase.DEPLOY, name = "upload-deb", requiresProject = true, requiresDirectInvocation = false, executionStrategy = "once-per-session", threadSafe = false)
 @Description("Upload debian packages to repository")
-public class UploadDeb extends AbstractSSHUploadMojo {
+public class UploadDeb extends AbstractSSHUploadMojo implements FileTransferProgress {
 
 	/**
 	 * The maven project.
@@ -40,11 +41,42 @@ public class UploadDeb extends AbstractSSHUploadMojo {
 	@Parameter(defaultValue = "${project.build.directory}", property = "upload-deb.dir", required = true)
 	protected File dir;
 
+	private long total;
+	private String transferring;
+	private int pc;
+
 	@Override
 	protected void upload(SftpClientTask ssh) throws IOException, SshException, SftpStatusException, TransferCancelledException, PermissionDeniedException {
 		ssh.cd(repository + "/" + codename);
 		ssh.lcd(dir.getAbsolutePath());
-		ssh.putFiles(source.getPath() + "/*.deb");
+		ssh.putFiles(source.getPath() + "/*.deb", this);
+	}
+
+	@Override
+	public void started(long bytesTotal, String remoteFile) {
+		getLog().info("Starting to transfer " + remoteFile);
+		pc = -1;
+		transferring = remoteFile;
+		total = bytesTotal;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return false;
+	}
+
+	@Override
+	public void progressed(long bytesSoFar) {
+		int tpc = (int)(((double)bytesSoFar / (double)total ) * 100d);
+		if(tpc != pc) {
+			pc = tpc;
+			getLog().info(pc + "% complete");
+		}
+	}
+
+	@Override
+	public void completed() {
+		getLog().info("Transferred " + transferring);
 	}
 
 }
