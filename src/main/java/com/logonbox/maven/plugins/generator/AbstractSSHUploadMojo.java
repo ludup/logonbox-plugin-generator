af0a49afe48bf3956a320054d9a0da473729fc60
Brett Smith <brett@logonbox.com>
2021-05-17 15:27:26 +0100

    Switch to sshj (temporary) to decide if its synergy

--- src/main/java/com/logonbox/maven/plugins/generator/AbstractSSHUploadMojo.java
+++ src/main/java/com/logonbox/maven/plugins/generator/AbstractSSHUploadMojo.java
@@ -6,16 +6,19 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import com.sshtools.client.SshClient;
-import com.sshtools.client.sftp.SftpClientTask;
-import com.sshtools.client.sftp.TransferCancelledException;
-import com.sshtools.common.logger.Log;
-import com.sshtools.common.logger.Log.Level;
-import com.sshtools.common.permissions.PermissionDeniedException;
-import com.sshtools.common.sftp.SftpStatusException;
-import com.sshtools.common.ssh.SshException;
+//import com.sshtools.client.SshClient;
+//import com.sshtools.client.sftp.SftpClientTask;
+//import com.sshtools.client.sftp.TransferCancelledException;
+//import com.sshtools.common.logger.Log;
+//import com.sshtools.common.logger.Log.Level;
+//import com.sshtools.common.permissions.PermissionDeniedException;
+//import com.sshtools.common.sftp.SftpStatusException;
+//import com.sshtools.common.ssh.SshException;
 
-public abstract class AbstractSSHUploadMojo extends AbstractBaseExtensionsMojo {
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.SFTPClient;
+
+public abstract class AbstractSSHUploadMojo extends AbstractBaseExtensionsMojo /* implements FileTransferProgress */ {
 
 	@Parameter(property = "sshupload.host", required = true, defaultValue = "packager.hypersocket.io")
 	protected String host;
@@ -29,33 +32,80 @@ public abstract class AbstractSSHUploadMojo extends AbstractBaseExtensionsMojo {
 	@Parameter(property = "sshupload.password", required = true)
 	protected String password;
 
-	protected void onExecute() throws MojoExecutionException, MojoFailureException {
-		Log.getDefaultContext().enableConsole(Level.DEBUG);
-		
-		getLog().info("Uploading to SSH server");
-		try (SshClient ssh = new SshClient(host, port, username, password.toCharArray())) {
-			ssh.runTask(new SftpClientTask(ssh) {
-				protected void doSftp() {
-					try {
-						upload(this);
-					} catch (IOException | SshException | SftpStatusException | TransferCancelledException
-							| PermissionDeniedException e) {
-						throw new IllegalStateException("Failed to upload.", e);
-					}
-				}
-			});
-		} catch (IOException | SshException sshe) {
-			throw new MojoExecutionException("Failed to upload to SSH server.", sshe);
-		}
-		getLog().info("Uploaded to SSH server");
-	}
 
-	protected abstract void upload(SftpClientTask amazonS3) throws IOException, SshException, SftpStatusException,
-			TransferCancelledException, PermissionDeniedException;
+	private long total;
+	private String transferring;
+	private int pc;
 
 	@Override
 	protected boolean isSnapshotVersionAsBuildNumber() {
 		return true;
 	}
+
+	protected void onExecute() throws MojoExecutionException, MojoFailureException {
+//		Log.getDefaultContext().enableConsole(Level.DEBUG);
+		
+		getLog().info("Uploading to SSH server");
+
+		try(final SSHClient ssh = new SSHClient()) {
+	        ssh.loadKnownHosts();
+	        ssh.connect(host, port);
+        	ssh.authPassword(username, password);
+        	try(final SFTPClient sftp = ssh.newSFTPClient()) {
+        		upload(sftp);
+        	}
+		}
+		catch(IOException ioe) {
+			throw new MojoExecutionException("Failed to upload.", ioe);
+		}
+		
+//		try (SshClient ssh = new SshClient(host, port, username, password.toCharArray())) {
+//			ssh.runTask(new SftpClientTask(ssh) {
+//				protected void doSftp() {
+//					try {
+//						upload(this);
+//					} catch (IOException | SshException | SftpStatusException | TransferCancelledException
+//							| PermissionDeniedException e) {
+//						throw new IllegalStateException("Failed to upload.", e);
+//					}
+//				}
+//			});
+//		} catch (IOException | SshException sshe) {
+//			throw new MojoExecutionException("Failed to upload to SSH server.", sshe);
+//		}
+		getLog().info("Uploaded to SSH server");
+	}
+	
+	protected abstract void upload(SFTPClient amazonS3) throws IOException;
+
+//	protected abstract void upload(SftpClientTask amazonS3) throws IOException, SshException, SftpStatusException,
+//		TransferCancelledException, PermissionDeniedException;
+	
+//	@Override
+//	public void started(long bytesTotal, String remoteFile) {
+//		getLog().info("Starting to transfer " + remoteFile);
+//		pc = -1;
+//		transferring = remoteFile;
+//		total = bytesTotal;
+//	}
+//
+//	@Override
+//	public boolean isCancelled() {
+//		return false;
+//	}
+//
+//	@Override
+//	public void progressed(long bytesSoFar) {
+//		int tpc = (int)(((double)bytesSoFar / (double)total ) * 100d);
+//		if(tpc != pc) {
+//			pc = tpc;
+//			getLog().info(pc + "% complete");
+//		}
+//	}
+//
+//	@Override
+//	public void completed() {
+//		getLog().info("Transferred " + transferring);
+//	}
 
 }
--- src/main/java/com/logonbox/maven/plugins/generator/UploadDeb.java
+++ src/main/java/com/logonbox/maven/plugins/generator/UploadDeb.java
@@ -9,19 +9,23 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.sonatype.inject.Description;
 
-import com.sshtools.client.sftp.SftpClientTask;
-import com.sshtools.client.sftp.TransferCancelledException;
-import com.sshtools.client.tasks.FileTransferProgress;
-import com.sshtools.common.permissions.PermissionDeniedException;
-import com.sshtools.common.sftp.SftpStatusException;
-import com.sshtools.common.ssh.SshException;
+//import com.sshtools.client.sftp.SftpClientTask;
+//import com.sshtools.client.sftp.TransferCancelledException;
+//import com.sshtools.client.tasks.FileTransferProgress;
+//import com.sshtools.common.permissions.PermissionDeniedException;
+//import com.sshtools.common.sftp.SftpStatusException;
+//import com.sshtools.common.ssh.SshException;
+
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.xfer.FileSystemFile;
+import net.schmizz.sshj.xfer.LocalSourceFile;
 
 /**
  * Upload Deb packages to SSH server (packager).
  */
 @Mojo(defaultPhase = LifecyclePhase.DEPLOY, name = "upload-deb", requiresProject = true, requiresDirectInvocation = false, executionStrategy = "once-per-session", threadSafe = false)
 @Description("Upload debian packages to repository")
-public class UploadDeb extends AbstractSSHUploadMojo implements FileTransferProgress {
+public class UploadDeb extends AbstractSSHUploadMojo  {
 
 	/**
 	 * The maven project.
@@ -41,42 +45,18 @@ public class UploadDeb extends AbstractSSHUploadMojo implements FileTransferProg
 	@Parameter(defaultValue = "${project.build.directory}", property = "upload-deb.dir", required = true)
 	protected File dir;
 
-	private long total;
-	private String transferring;
-	private int pc;
-
+//	@Override
+//	protected void upload(SftpClientTask ssh) throws IOException, SshException, SftpStatusException, TransferCancelledException, PermissionDeniedException {
+//		ssh.cd(repository + "/" + codename);
+//		ssh.lcd(dir.getAbsolutePath());
+//		ssh.putFiles(source.getPath() + "/*.deb", this);
+//	}
 	@Override
-	protected void upload(SftpClientTask ssh) throws IOException, SshException, SftpStatusException, TransferCancelledException, PermissionDeniedException {
-		ssh.cd(repository + "/" + codename);
-		ssh.lcd(dir.getAbsolutePath());
-		ssh.putFiles(source.getPath() + "/*.deb", this);
-	}
-
-	@Override
-	public void started(long bytesTotal, String remoteFile) {
-		getLog().info("Starting to transfer " + remoteFile);
-		pc = -1;
-		transferring = remoteFile;
-		total = bytesTotal;
-	}
-
-	@Override
-	public boolean isCancelled() {
-		return false;
-	}
-
-	@Override
-	public void progressed(long bytesSoFar) {
-		int tpc = (int)(((double)bytesSoFar / (double)total ) * 100d);
-		if(tpc != pc) {
-			pc = tpc;
-			getLog().info(pc + "% complete");
+	protected void upload(SFTPClient ssh) throws IOException {
+		for(File file : source.listFiles()) {
+			if(file.getName().endsWith(".deb")) {
+				ssh.put(new FileSystemFile(file), repository + "/" + codename);
+			}
 		}
 	}
-
-	@Override
-	public void completed() {
-		getLog().info("Transferred " + transferring);
-	}
-
 }
